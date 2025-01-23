88173cc6fefe51e77458acea80912289aa0b82f2
양은서 <dmstj4871@naver.com>
2025-01-23 15:05:22 +0900

    feat(S12P11A404-105): 로그인 API 구현

--- server/src/main/java/com/kiosk/server/user/controller/UserController.java
+++ server/src/main/java/com/kiosk/server/user/controller/UserController.java
@@ -1,11 +1,16 @@
 package com.kiosk.server.user.controller;
 
+import com.kiosk.server.user.controller.dto.LoginRequest;
 import com.kiosk.server.user.controller.dto.RegisterRequest;
 import com.kiosk.server.user.service.RegisterUserService;
+import com.kiosk.server.user.service.UserLoginService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
@@ -13,11 +18,21 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final RegisterUserService registerUserService;
+    private final UserLoginService userLoginService;
 
     @PostMapping("/user")
     public ResponseEntity<Void> register(@RequestBody RegisterRequest request) {
         registerUserService.doService(request.email(), request.password());
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PostMapping("/user/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest request) {
+        String temporaryToken = userLoginService.doService(request.email(), request.password());
+        Map<String, Object> response = new HashMap<>();
+        response.put("accessToken", temporaryToken);
+        response.put("tokenType", "Bearer");
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
 }
---
+++ server/src/main/java/com/kiosk/server/user/controller/dto/LoginRequest.java
@@ -0,0 +1,4 @@
+package com.kiosk.server.user.controller.dto;
+
+public record LoginRequest(String email, String password) {
+}
