3e82f15f3412e25cd68969c04f257a89c4506e95
EunSeo Yang <yes24150@gmail.com>
2025-01-25 12:50:31 +0900

    refactor(S12P11A404-105): User 생성 메서드에 역할(role) 지정 추가 - create 메서드에 UserRole 파라미터를 추가하여 역할 지정 가능하도록 수정 - 기존 하드코딩된 역할 UserRole.CUSTOMER를 제거

--- server/src/main/java/com/kiosk/server/user/domain/User.java
+++ server/src/main/java/com/kiosk/server/user/domain/User.java
@@ -16,10 +16,10 @@ public class User {
     private String salt;
     private LocalDateTime regDate;
 
-    public static User create(String email, String password) {
+    public static User create(String email, String password, UserRole role) {
         User user = new User();
         user.userId = IdUtil.create();
-        user.role = UserRole.CUSTOMER;
+        user.role = role;
         user.email = email;
         user.salt = HashUtil.generateSalt();
         user.password = HashUtil.hashPassword(password, user.salt);
