a880aac7f952249b7ee6517166813574718f8b3c
EunSeo Yang <yes24150@gmail.com>
2025-01-25 12:51:50 +0900

    refactor(S12P11A404-105): UserRole에 문자열 변환 유틸리티 메서드 추가 - String -> UserRole 변환 메서드 추가 (fromString) - UserRole -> String 변환 메서드 추가 (toString) - 변환 실패 시 UnauthorizedException을 발생하도록 처리

--- server/src/main/java/com/kiosk/server/user/domain/UserRole.java
+++ server/src/main/java/com/kiosk/server/user/domain/UserRole.java
@@ -1,5 +1,23 @@
 package com.kiosk.server.user.domain;
 
+import com.kiosk.server.common.exception.custom.UnauthorizedException;
+
 public enum UserRole {
-    CUSTOMER, CEO, MANAGER
+    CUSTOMER, CEO, MANAGER;
+
+    // String -> UserRole 변환
+    public static UserRole fromString(String role) {
+        try {
+            return UserRole.valueOf(role.toUpperCase());
+        } catch (final UnauthorizedException e) {
+            throw new UnauthorizedException("Invalid user role: " + role);
+        }
+    }
+
+
+    // UserRole -> String
+    public String toString() {
+        return this.name();
+    }
+
 }
