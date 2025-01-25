bb487dadea67935dda3b259edc3d5e28116b7777
EunSeo Yang <yes24150@gmail.com>
2025-01-25 16:20:56 +0900

    refactor(S12P11A404-105): AuthController에서 회원가입 로직 분리

--- server/src/main/java/com/kiosk/server/user/controller/AuthController.java
+++ server/src/main/java/com/kiosk/server/user/controller/AuthController.java
@@ -1,28 +1,22 @@
 package com.kiosk.server.user.controller;
 
-import com.kiosk.server.user.controller.dto.UserLoginRequest;
 import com.kiosk.server.user.controller.dto.LoginResponse;
-import com.kiosk.server.user.controller.dto.RegisterUserRequest;
-import com.kiosk.server.user.service.RegisterUserService;
+import com.kiosk.server.user.controller.dto.UserLoginRequest;
 import com.kiosk.server.user.service.UserLoginService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final RegisterUserService registerUserService;
     private final UserLoginService userLoginService;
-
-    @PostMapping("/user")
-    public ResponseEntity<Void> register(@RequestBody RegisterUserRequest request) {
-        registerUserService.doService(request.email(), request.password());
-        return ResponseEntity.status(HttpStatus.CREATED).build();
-    }
 
     @PostMapping("/user/login")
     public ResponseEntity<LoginResponse> login(@RequestBody UserLoginRequest request) {
