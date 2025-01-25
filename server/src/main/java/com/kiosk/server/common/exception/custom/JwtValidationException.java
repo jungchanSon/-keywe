d138e6ae56b43eec679c88eba59aa60a5e0a9240
EunSeo Yang <yes24150@gmail.com>
2025-01-25 12:35:41 +0900

    remove(S12P11A404-105): 처리 기능이 동일한 Exception 삭제

--- server/src/main/java/com/kiosk/server/common/exception/custom/JwtValidationException.java
+++
@@ -1,7 +0,0 @@
-package com.kiosk.server.common.exception.custom;
-
-public class JwtValidationException extends RuntimeException {
-    public JwtValidationException(String message) {
-        super(message);
-    }
-}
