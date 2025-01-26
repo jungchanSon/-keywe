f7a696ca8d27df1bd7e63fa95f01f643687513e4
EunSeo Yang <yes24150@gmail.com>
2025-01-27 01:02:36 +0900

    feat(S12P11A404-121): JWT 토큰 검증 및 권한 관리를 위한 필터와 인터셉터 구현

--- server/src/main/java/com/kiosk/server/common/config/FilterConfig.java
+++ server/src/main/java/com/kiosk/server/common/config/FilterConfig.java
@@ -5,8 +5,6 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
-
 @Configuration
 public class FilterConfig {
 
