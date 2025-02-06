c346d740f15cbea150c2328d2c9d1c3ebc78f41f
csct3434 <csct3434@gmail.com>
2025-02-06 23:40:58 +0900

    merge(S12P11A404-157): dev/be 브랜치 충돌 해결

--- server/infra-gateway/src/main/java/com/kiosk/server/config/GatewayConfig.java
+++ server/infra-gateway/src/main/java/com/kiosk/server/config/GatewayConfig.java
@@ -1,5 +1,6 @@
 package com.kiosk.server.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -8,17 +9,18 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GatewayConfig {
 
-    private final ServiceUrlProperty serviceUrlProperty;
+    @Value("${GATEWAY.USER_SERVICE_URL}")
+    String USER_SERVICE_URL;
 
-    public GatewayConfig(ServiceUrlProperty serviceUrlProperty) {
-        this.serviceUrlProperty = serviceUrlProperty;
-    }
+    @Value("${GATEWAY.STORE_SERVICE_URL}")
+    String STORE_SERVICE_URL;
+
 
     @Bean
     public RouteLocator routeLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-            .route(r -> r.path("/user/**").uri(serviceUrlProperty.userService()))
-            .route(r -> r.path("/store/**", "/menu/**", "/category/**").uri(serviceUrlProperty.storeService()))
+            .route(r -> r.path("/user/**").uri(USER_SERVICE_URL))
+            .route(r -> r.path("/store/**", "/menu/**", "/category/**").uri(STORE_SERVICE_URL))
             .build();
     }
 }
