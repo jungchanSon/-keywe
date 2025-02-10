8bb27a29627f2564e5703d341161dc0390b2fc14
csct3434 <csct3434@gmail.com>
2025-02-10 13:16:45 +0900

    fix: swagger CORS 오류

--- server/infra-gateway/src/main/java/com/kiosk/server/config/GatewayCorsConfig.java
+++ server/infra-gateway/src/main/java/com/kiosk/server/config/GatewayCorsConfig.java
@@ -6,6 +6,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Configuration
@@ -14,13 +15,14 @@ public class GatewayCorsConfig {
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:8080"));
+        config.setAllowedOriginPatterns(List.of("http://localhost:8080/**", "http://i12a404.p.ssafy.io:8080/**"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        config.setAllowedHeaders(List.of("*"));
+        config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
+        source.registerCorsConfiguration("/swagger-ui/**", config);
+        source.registerCorsConfiguration("/v3/api-docs/**", config);
 
         return new CorsWebFilter(source);
     }
