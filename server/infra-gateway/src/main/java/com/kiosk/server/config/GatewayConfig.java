9c5d4874c68d420c73236a9e9682717cda9b7aaf
csct3434 <csct3434@gmail.com>
2025-02-10 10:35:26 +0900

    feat(S12P11A404-153): API Gateway 라우팅 추가 - Remote Service

--- server/infra-gateway/src/main/java/com/kiosk/server/config/GatewayConfig.java
+++ server/infra-gateway/src/main/java/com/kiosk/server/config/GatewayConfig.java
@@ -1,26 +1,23 @@
 package com.kiosk.server.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class GatewayConfig {
 
-    @Value("${GATEWAY.USER_SERVICE_URL}")
-    String USER_SERVICE_URL;
-
-    @Value("${GATEWAY.STORE_SERVICE_URL}")
-    String STORE_SERVICE_URL;
-
+    private final ServiceUrlProperties serviceUrlProperties;
 
     @Bean
     public RouteLocator routeLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-            .route(r -> r.path("/user/**", "/auth/**").uri(USER_SERVICE_URL))
-            .route(r -> r.path("/store/**", "/menu/**", "/category/**").uri(STORE_SERVICE_URL))
+            .route(r -> r.path("/user/**", "/auth/**").uri(serviceUrlProperties.userService()))
+            .route(r -> r.path("/store/**", "/menu/**", "/category/**").uri(serviceUrlProperties.storeService()))
+            .route(r -> r.path("/remote/**").uri(serviceUrlProperties.remoteService()))
             .build();
     }
 }
