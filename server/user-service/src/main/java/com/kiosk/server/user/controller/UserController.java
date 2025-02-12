4f7006a5e84e85d5a4771e770872f9c188aeb49f
EunSeo Yang <yes24150@gmail.com>
2025-02-12 22:51:16 +0900

    fix: CreateProfileRequest에서 request.type()을 request.role()로 수정하여 올바른 필드 사용

--- server/user-service/src/main/java/com/kiosk/server/user/controller/UserController.java
+++ server/user-service/src/main/java/com/kiosk/server/user/controller/UserController.java
@@ -46,7 +46,7 @@ public class UserController {
     // 프로필 생성
     @PostMapping("/profile")
     public ResponseEntity<CreateProfileResponse> createProfile(@RequestHeader("userId") Long userId, @RequestBody CreateProfileRequest request) {
-        CreateProfileResponse response = createUserProfileService.doService(userId, request.name(), request.type(), request.phone(), request.password());
+        CreateProfileResponse response = createUserProfileService.doService(userId, request.name(), request.role(), request.phone(), request.password());
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
