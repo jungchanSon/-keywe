7856f21d7d0202937e67b7f6632037492931fa73
EunSeo Yang <yes24150@gmail.com>
2025-02-13 22:44:32 +0900

    feat(S12P11A404-259): user-service Controller Logger - Response 삭제

--- server/user-service/src/main/java/com/kiosk/server/user/controller/UserController.java
+++ server/user-service/src/main/java/com/kiosk/server/user/controller/UserController.java
@@ -27,7 +27,7 @@ public class UserController {
     // 회원가입
     @PostMapping
     public ResponseEntity<Void> register(@RequestBody RegisterUserRequest request) {
-        log.info("request: email={}", request.email());
+        log.info("Request: email={}", request.email());
         registerUserService.doService(request.email(), request.password());
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
@@ -35,27 +35,24 @@ public class UserController {
     // 프로필 목록 조회
     @GetMapping("/profile/list")
     public ResponseEntity<List<UserProfileResponse>> getUserProfileList(@RequestHeader("userId") Long userId) {
-        log.info("request: userId={}", userId);
+        log.info("Request: userId={}", userId);
         List<UserProfileResponse> userProfileList = getUserProfileListService.doService(userId);
-        log.info("response: userId={}, profilesCount={}", userId, userProfileList.size());
         return ResponseEntity.ok(userProfileList);
     }
 
     // 프로필 상세조회
     @GetMapping("/profile")
     public ResponseEntity<UserProfileDetailResponse> getUserProfile(@RequestHeader("userId") Long userId, @RequestHeader("profileId") Long profileId) {
-        log.info("request: userId={}, profileId={}", userId, profileId);
+        log.info("Request: userId={}, profileId={}", userId, profileId);
         UserProfileDetailResponse response = profileDetailService.doService(userId, profileId);
-        log.info("response: profileId={}, name={}, role={}", profileId, response.name(), response.role());
         return ResponseEntity.ok(response);
     }
 
     // 프로필 생성
     @PostMapping("/profile")
     public ResponseEntity<CreateProfileResponse> createProfile(@RequestHeader("userId") Long userId, @RequestBody CreateProfileRequest request) {
-        log.info("request: userId={}, name={}, role={}", userId, request.name(), request.role());
+        log.info("Request: userId={}, name={}, role={}", userId, request.name(), request.role());
         CreateProfileResponse response = createUserProfileService.doService(userId, request.name(), request.role(), request.phone(), request.password());
-        log.info("response: userId={}, profileId={}, createdAt={}", userId, response.id(),response.createAt());
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
@@ -66,9 +63,8 @@ public class UserController {
             @RequestHeader("profileId") Long profileId,
             @RequestBody PatchProfileRequest request
     ) {
-        log.info("request: userId={}, profileId={}, name={}, phone={}", userId, profileId, request.name(), request.phone());
+        log.info("Request: userId={}, profileId={}, name={}, phone={}", userId, profileId, request.name(), request.phone());
         PatchProfileResponse response = modifyUserProfileService.doService(userId, profileId, request.name(), request.phone(), request.password());
-        log.info("response:profileId={}, name={}, phone={}", profileId, response.name(), response.phone());
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
@@ -79,9 +75,8 @@ public class UserController {
             @RequestHeader("profileId") Long originProfileId,
             @Validated @RequestBody UserProfileRequest request
     ) {
-        log.info("request: userId={}, originProfileId={}, targetProfileId={}", userId, originProfileId, request.profileId());
+        log.info("Request: userId={}, originProfileId={}, targetProfileId={}", userId, originProfileId, request.profileId());
         deleteUserProfileService.doService(userId, originProfileId, request.profileId());
-        log.info("response: profileId={}", request.profileId());
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
