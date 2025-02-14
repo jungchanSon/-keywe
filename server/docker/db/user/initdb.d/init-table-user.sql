21af63ae2f27d25bbef789780b36cc25df8ee7d8
양은서 <dmstj4871@naver.com>
2025-02-14 17:41:29 +0900

    feat(S12P11A404-219): Users DB profile_image 테이블 DDL 추가
    - profile_image 테이블 신규 생성 (image_id, user_id, image_bytes, created_at 등)
    - user_id 컬럼에 users 테이블과 연동되는 외래키 설정
    - 사용자 이미지 저장을 위한 DDL 구조 정의

--- server/docker/db/user/initdb.d/init-table-user.sql
+++ server/docker/db/user/initdb.d/init-table-user.sql
@@ -7,7 +7,7 @@ create table if not exists users
     primary key,
     role     varchar(50)  not null,
     email    varchar(255) not null,
-    password varchar(255) null,
+    password varchar(255) not null,
     salt     varchar(255) not null,
     reg_date datetime     not null
     );
@@ -19,10 +19,21 @@ create table if not exists user_profile
     user_id      bigint       not null,
     profile_name varchar(255) not null,
     profile_role varchar(50)  not null,
-    phone_number varchar(50)  not null,
+    phone_number varchar(50)  null,
     profile_pass varchar(10)  null,
     reg_date     datetime     not null,
     constraint user_profile_fk
     foreign key (user_id) references users (user_id)
     on update cascade on delete cascade
     );
+
+create table if not exists profile_image
+(
+    image_id    bigint     not null
+        primary key,
+    user_id     bigint     not null,
+    profile_id  bigint     not null,
+    image_bytes mediumblob not null,
+    created_at  datetime   not null
+);
+
