80276cd08ee70a22b2dacbbb1a37c6a779138179
양은서 <dmstj4871@naver.com>
2025-02-14 18:33:09 +0900

    feat(S12P11A404-219): 프로필 조회 시 프로필별 이미지 불러오는 기능 추가

--- server/user-service/src/main/java/com/kiosk/server/user/service/GetUserProfileListService.java
+++ server/user-service/src/main/java/com/kiosk/server/user/service/GetUserProfileListService.java
@@ -1,10 +1,10 @@
 package com.kiosk.server.user.service;
 
-import com.kiosk.server.user.controller.dto.UserProfileResponse;
+import com.kiosk.server.user.controller.dto.UserProfileListResponse;
 
 import java.util.List;
 
 public interface GetUserProfileListService {
 
-    List<UserProfileResponse> doService(long userId);
+    List<UserProfileListResponse> doService(long userId);
 }
