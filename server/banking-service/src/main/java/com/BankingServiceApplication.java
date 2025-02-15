38dda687929ff4c5d372f5456ab2925444004b1e
jungchanSon <rnrmfjc@gmail.com>
2025-02-16 03:43:22 +0900

    hotfix: 배포환경 테스트

--- server/banking-service/src/main/java/com/BankingServiceApplication.java
+++ server/banking-service/src/main/java/com/BankingServiceApplication.java
@@ -1,13 +1,11 @@
-package com.kiosk;
+package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.kiosk.server"})
 public class BankingServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(BankingServiceApplication.class, args);
