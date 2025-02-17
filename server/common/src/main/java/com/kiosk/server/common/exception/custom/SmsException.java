ca0d56135ee5964379986a38cc5ebb6e54c79b06
양은서 <dmstj4871@naver.com>
2025-02-17 10:34:49 +0900

    feat(S12P11A404-294): SMS 전송 예외 처리를 위한 SmsException 추가

---
+++ server/common/src/main/java/com/kiosk/server/common/exception/custom/SmsException.java
@@ -0,0 +1,8 @@
+package com.kiosk.server.common.exception.custom;
+
+public class SmsException extends RuntimeException {
+
+    public SmsException(String message) {
+        super(message);
+    }
+}
