2fa01cb442f8227dbb94c67ac97de6118e903bfb
김정현 <invent819@naver.com>
2025-02-19 21:33:55 +0900

    feat: add default menu back dialog

--- android/app/src/main/java/com/ssafy/keywe/presentation/order/DefaultMenuScreen.kt
+++ android/app/src/main/java/com/ssafy/keywe/presentation/order/DefaultMenuScreen.kt
@@ -1,23 +1,33 @@
 package com.ssafy.keywe.presentation.order
 
 import android.annotation.SuppressLint
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Scaffold
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
@@ -25,17 +35,21 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.zIndex
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.ssafy.keywe.R
 import com.ssafy.keywe.common.Route
-import com.ssafy.keywe.common.app.DefaultAppBar
+import com.ssafy.keywe.common.app.isOnlyCurrentScreenInBackStack
+import com.ssafy.keywe.presentation.order.component.MenuCartDeleteDialog
 import com.ssafy.keywe.presentation.order.component.MenuCategoryScreen
 import com.ssafy.keywe.presentation.order.component.MenuMenuList
 import com.ssafy.keywe.presentation.order.component.MenuSubCategory
 import com.ssafy.keywe.presentation.order.viewmodel.MenuCartViewModel
 import com.ssafy.keywe.presentation.order.viewmodel.MenuViewModel
+import com.ssafy.keywe.ui.theme.h6
 import com.ssafy.keywe.ui.theme.primaryColor
+import com.ssafy.keywe.ui.theme.titleTextColor
 import com.ssafy.keywe.ui.theme.whiteBackgroundColor
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
@@ -46,18 +60,62 @@ fun DefaultMenuScreen(
     menuCartViewModel: MenuCartViewModel = hiltViewModel(),
     storeId: Long,
 ) {
-//    Log.d("MenuSCreen", "storeId = $storeId")
 
-    Scaffold(
-        topBar = {
-            DefaultAppBar(
-                title = "주문하기",
-                navController = navController,
+    var showDialog by remember { mutableStateOf(false) }
+
+    BackHandler {
+        showDialog = true
+    }
+
+
+    Box(
+        modifier = Modifier
+            .zIndex(1f)
+            .fillMaxSize()
+            .background(
+                color = if (showDialog) titleTextColor.copy(
+                    alpha = 0.5f
+                ) else Color.Transparent
             )
-        },
-        modifier = Modifier.fillMaxSize(), floatingActionButton = {
-            DefaultFloatingCartButton(navController, menuCartViewModel, storeId)
-        }) {
+    ) {
+        if (showDialog) {
+            MenuCartDeleteDialog(title = "주문 종료",
+                description = "주문 종료시 장바구니는 초기화 됩니다.",
+                onCancel = {
+                    showDialog = false
+                },
+                onConfirm = {
+                    /* 너의 action */
+                    showDialog = false
+                    navController.popBackStack()
+                })
+        }
+    }
+
+    Scaffold(topBar = {
+
+
+        TopAppBar(
+            backgroundColor = whiteBackgroundColor,
+            elevation = 0.dp,
+            windowInsets = WindowInsets(0, 0, 0, 0),
+            title = { Text(text = "주문하기", style = h6) },
+            navigationIcon = {
+                if (!isOnlyCurrentScreenInBackStack(navController)) {
+                    IconButton(onClick = {
+                        showDialog = true
+                    }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.KeyboardArrowLeft,
+                            contentDescription = null
+                        )
+                    }
+                }
+            },
+        )
+    }, modifier = Modifier.fillMaxSize(), floatingActionButton = {
+        DefaultFloatingCartButton(navController, menuCartViewModel, storeId)
+    }) {
         Column(
             modifier = Modifier.fillMaxHeight()
         ) {
@@ -82,7 +140,7 @@ fun DefaultFloatingCartButton(
     menuCartViewModel: MenuCartViewModel,
     storeId: Long,
 
-) {
+    ) {
     val cartItems by menuCartViewModel.cartItems.collectAsState()
     val cartItemsCount = cartItems.sumOf { it.quantity }
 
